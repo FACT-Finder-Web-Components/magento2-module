4ef61d6efa96bdd90647b7ca810a1d7d0c409108
Jaroslaw Weselski <jweselski@objectivity.co.uk>
2018-09-05 13:14:14 +0200

    Replace redirect script

--- Omikron/Factfinder/view/frontend/web/js/search-redirect.js
+++ Omikron/Factfinder/view/frontend/web/js/search-redirect.js
@@ -1,53 +1,15 @@
+document.addEventListener("ffReady", function () {
+    factfinder.communication.FFCommunicationEventAggregator.addBeforeDispatchingCallback(function (event) {
+        var urlArr = window.location.href.split("/");
+        var domainUrl = urlArr[0] + "//" + urlArr[2];
+        var redirectPath = '/FACT-Finder/result/';
+        if (event.type === "search" && window.location.href.match(/FACT-Finder\/result/) === null) {
+            delete event.type;
+            delete event.version;
 
-require(["jquery"], function($){
-    //jquery v1.11
-
-    (function() {
-
-        'use strict';
-
-        var redirectPath = '/FACT-Finder/result';
-        var query_minLength = 1;
-
-        var searchBoxEl;
-
-        $(document).ready(function() {
-
-            searchBoxEl = $("input[is='ff-searchbox']").first();
-
-            searchBoxEl.keypress(function (e) {
-
-                if (e.which == 13) { //enterKey pressed
-                    ffRedirect();
-                    return false;
-                }
-
-            });
-
-            $("[is='ff-searchbutton']").each(function(){
-
-                $(this).on( "click", function() {
-                    ffRedirect();
-                });
-
-            });
-
-        });
-
-        window.ffRedirect = function() {
-
-            var urlArr = window.location.href.split("/");
-            var domainUrl = urlArr[0] + "//" + urlArr[2];
-
-            var query = searchBoxEl.val();
-
-            if(query.length >= query_minLength) {
-                window.location.href = domainUrl + redirectPath + '?query=' + encodeURIComponent(query);
-            }
-
+            var params = factfinder.common.dictToParameterString(event);
+            window.location.href = domainUrl + redirectPath + params;
+            delete event.type;
         }
-
-    })(this);
-
-
+    });
 });
