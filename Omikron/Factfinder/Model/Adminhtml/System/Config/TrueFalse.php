aa9c19d9d3b49ec3b9e60577221cfed1afed6f65
a-laurowski <Panpiczek1!>
2019-02-19 11:16:47 +0100

    Change TrueFalse to be source model

--- Omikron/Factfinder/Model/Adminhtml/System/Config/TrueFalse.php
+++ Omikron/Factfinder/Model/Adminhtml/System/Config/TrueFalse.php
@@ -4,29 +4,12 @@ declare(strict_types=1);
 
 namespace Omikron\Factfinder\Model\Adminhtml\System\Config;
 
-use Magento\Framework\App\Config\Value;
+use Magento\Framework\Data\OptionSourceInterface;
 
-class TrueFalse extends Value
+class TrueFalse implements OptionSourceInterface
 {
-    public function beforeSave()
+    public function toOptionArray(): array
     {
-        if ((bool) $this->getValue()) {
-            $this->setValue('true');
-        } else {
-            $this->setValue('false');
-        }
-
-        return parent::beforeSave();
-    }
-
-    public function afterLoad()
-    {
-        if ($this->getValue() === 'true') {
-            $this->setValue('1');
-        } else {
-            $this->setValue('0');
-        }
-
-        return parent::afterLoad();
+        return [['value' => 'true', 'label' => __('Yes')], ['value' => 'false', 'label' => __('No')]];
     }
 }
