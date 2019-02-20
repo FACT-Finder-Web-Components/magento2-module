28161375b14850be56281789ca0a69fe1e173d98
Francesco Marangi <francesco.maragi@omikron.net>
2019-02-20 13:20:22 +0100

    Fix unit test

--- Omikron/Factfinder/Test/Unit/Model/Config/CommunicationParametersProviderTest.php
+++ Omikron/Factfinder/Test/Unit/Model/Config/CommunicationParametersProviderTest.php
@@ -10,34 +10,29 @@ use PHPUnit\Framework\TestCase;
 class CommunicationParametersProviderTest extends TestCase
 {
     /**
-     * @testdox InvalidArgumentException should be thrown if source is not implementing ParametersSourceInterface
+     * @testdox A TypeError should be thrown if source is not implementing ParametersSourceInterface
      */
     public function test_get_parameters_thrown_an_exception_if_source_is_wrong_type()
     {
-        $parametersProvider = new CommunicationParametersProvider(
-            [
-                $this->createMock(ParametersSourceInterface::class),
-                $this->createMock(ParametersSourceInterface::class),
-                'Not a object'
-            ]
-        );
+        $parametersProvider = new CommunicationParametersProvider([
+            $this->createMock(ParametersSourceInterface::class),
+            $this->createMock(ParametersSourceInterface::class),
+            'Not a object',
+        ]);
 
-        $this->expectException(\InvalidArgumentException::class);
+        $this->expectException(\TypeError::class);
         $parametersProvider->getParameters();
     }
 
     public function test_get_parameters_should_merge_parameters_from_all_providers()
     {
-        $parametersProvider = new CommunicationParametersProvider(
-            [
-                $this->createConfiguredMock(ParametersSourceInterface::class, ['getParameters' => ['param1' => 'value1']]),
-                $this->createConfiguredMock(ParametersSourceInterface::class, ['getParameters' => ['param2' => 'value2']]),
-                $this->createConfiguredMock(ParametersSourceInterface::class, ['getParameters' => ['param3' => 'value3']]),
-            ]
-        );
+        $parametersProvider = new CommunicationParametersProvider([
+            $this->createConfiguredMock(ParametersSourceInterface::class, ['getParameters' => ['param1' => 'value1']]),
+            $this->createConfiguredMock(ParametersSourceInterface::class, ['getParameters' => ['param2' => 'value2']]),
+            $this->createConfiguredMock(ParametersSourceInterface::class, ['getParameters' => ['param3' => 'value3']]),
+        ]);
 
         $parameters = $parametersProvider->getParameters();
-
         $this->assertSame(['param1' => 'value1', 'param2' => 'value2', 'param3' => 'value3'], $parameters);
     }
 }
