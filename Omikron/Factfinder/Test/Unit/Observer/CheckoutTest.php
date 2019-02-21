712154f2cc5a6f0e92d4117562cbefd0f6bb2b6e
a.laurowski <alaurowski.objectivity@gmail.com>
2019-02-21 16:52:45 +0100

    Add unit tests for tracking observers (#105)

---
+++ Omikron/Factfinder/Test/Unit/Observer/CheckoutTest.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Omikron\Factfinder\Observer\Tracking;
+
+use Magento\Catalog\Model\Product;
+use Magento\Framework\Event\Observer;
+use Magento\Quote\Model\Quote;
+use Magento\Quote\Model\Quote\Item;
+use Magento\Store\Model\StoreManagerInterface;
+use Omikron\Factfinder\Api\Data\TrackingProductInterface;
+use Omikron\Factfinder\Api\Data\TrackingProductInterfaceFactory;
+use Omikron\Factfinder\Api\FieldRolesInterface;
+use Omikron\Factfinder\Helper\Product as ProductHelper;
+use Omikron\Factfinder\Model\Api\Tracking;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+class CheckoutTest extends TestCase
+{
+    /** @var MockObject|Tracking */
+    private $trackingMock;
+
+    /** @var MockObject|TrackingProductInterfaceFactory */
+    private $trackingProductFactoryMock;
+
+    /** @var Checkout */
+    private $checkoutObserver;
+
+    public function test_execute_should_call_tracking_with_parameters_of_correct_type()
+    {
+        $quoteMock = $this->createConfiguredMock(Quote::class, ['getAllVisibleItems' => [
+            $this->createConfiguredMock(Item::class, ['getProduct' => $this->createMock(Product::class)]),
+            $this->createConfiguredMock(Item::class, ['getProduct' => $this->createMock(Product::class)]),
+        ]]);
+
+        $this->trackingProductFactoryMock->expects($this->atLeastOnce())
+            ->method('create')
+            ->willReturn($this->getMockBuilder(TrackingProductInterface::class)->getMock());
+
+        $this->trackingMock->expects($this->once())
+            ->method('execute')
+            ->with($this->isType('string'), $this->isInstanceOf(TrackingProductInterface::class));
+
+        $this->checkoutObserver->execute(new Observer(['quote' => $quoteMock]));
+    }
+
+    protected function setUp()
+    {
+        $this->trackingMock               = $this->createMock(Tracking::class);
+        $this->trackingProductFactoryMock = $this->createMock(TrackingProductInterfaceFactory::class);
+
+        $this->checkoutObserver = new Checkout(
+            $this->trackingMock,
+            $this->trackingProductFactoryMock,
+            $this->createMock(ProductHelper::class),
+            $this->createMock(FieldRolesInterface::class),
+            $this->createMock(StoreManagerInterface::class)
+        );
+    }
+}
