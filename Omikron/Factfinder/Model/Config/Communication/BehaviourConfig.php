604a8224c8516fe40ce367cfdd8c8a8fff3703a2
a.laurowski <alaurowski.objectivity@gmail.com>
2019-02-25 12:59:58 +0100

    FFWEB-869: Update FACT-Finder Web Components to version 3.0 (#107)

--- Omikron/Factfinder/Model/Config/Communication/BehaviourConfig.php
+++ Omikron/Factfinder/Model/Config/Communication/BehaviourConfig.php
@@ -14,7 +14,6 @@ class BehaviourConfig implements ParametersSourceInterface
     private const PATH_ADD_PARAMS                  = 'factfinder/advanced/add_params';
     private const PATH_ADD_TRACKING_PARAMS         = 'factfinder/advanced/add_tracking_params';
     private const PATH_KEEP_URL_PARAMS             = 'factfinder/advanced/keep_url_param';
-    private const PATH_SEARCH_IMMEDIATE            = 'factfinder/advanced/search_immediate';
     private const PATH_DISABLE_SINGLE_HIT_REDIRECT = 'factfinder/advanced/disable_single_hit_redirect';
     private const PATH_ONLY_SEARCH_PARAMS          = 'factfinder/advanced/only_search_params';
     private const PATH_PARAMETER_WHITELIST         = 'factfinder/advanced/parameter_whitelist';
@@ -34,9 +33,8 @@ class BehaviourConfig implements ParametersSourceInterface
             'add-params'                  => $this->scopeConfig->getValue(self::PATH_ADD_PARAMS, ScopeInterface::SCOPE_STORES),
             'add-tracking-params'         => $this->scopeConfig->getValue(self::PATH_ADD_TRACKING_PARAMS, ScopeInterface::SCOPE_STORES),
             'keep-url-params'             => $this->scopeConfig->getValue(self::PATH_KEEP_URL_PARAMS, ScopeInterface::SCOPE_STORES),
-            'only-search-params'          => $this->scopeConfig->isSetFlag(self::PATH_ONLY_SEARCH_PARAMS, ScopeInterface::SCOPE_STORES),
+            'only-search-params'          => $this->scopeConfig->getValue(self::PATH_ONLY_SEARCH_PARAMS, ScopeInterface::SCOPE_STORES),
             'parameter-whitelist'         => $this->scopeConfig->getValue(self::PATH_PARAMETER_WHITELIST, ScopeInterface::SCOPE_STORES),
-            'search-immediate'            => $this->scopeConfig->isSetFlag(self::PATH_SEARCH_IMMEDIATE, ScopeInterface::SCOPE_STORES),
             'disable-single-hit-redirect' => $this->scopeConfig->getValue(self::PATH_DISABLE_SINGLE_HIT_REDIRECT, ScopeInterface::SCOPE_STORES),
         ];
     }
