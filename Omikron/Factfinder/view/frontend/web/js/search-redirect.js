d3eff3029a29422470d22683f1d2ddc0f152e548
Francesco Marangi <francesco.maragi@omikron.net>
2019-02-25 10:27:58 +0100

    Refactor layout XML, drop support for SEO configuration

--- Omikron/Factfinder/view/frontend/web/js/search-redirect.js
+++ Omikron/Factfinder/view/frontend/web/js/search-redirect.js
@@ -1,10 +1,10 @@
-document.addEventListener("ffReady", function () {
+document.addEventListener('ffReady', function () {
     factfinder.communication.FFCommunicationEventAggregator.addBeforeDispatchingCallback(function (event) {
-        var urlArr = window.location.href.split("/");
-        var domainUrl = urlArr[0] + "//" + urlArr[2];
+        var urlArr = window.location.href.split('/');
+        var domainUrl = urlArr[0] + '//' + urlArr[2];
         var redirectPath = '/FACT-Finder/result/';
 
-        if (event.type === "search" && window.location.href.match(/FACT-Finder\/result/) === null) {
+        if (event.type === 'search' && window.location.href.match(/FACT-Finder\/result/) === null) {
             var params = factfinder.common.dictToParameterString(event);
             window.location.href = domainUrl + redirectPath + params;
         }
