7931641f83543a629d1df4eb03a473385e969772
a-laurowski <Panpiczek1!>
2019-02-25 09:28:26 +0100

    Replace attached web components script extensions to min.js, Retrieve removed styles.phtml, remove unecessary custom-style, removed search-redirect.js lines which deletes event.type and event.version

--- Omikron/Factfinder/view/frontend/web/js/search-redirect.js
+++ Omikron/Factfinder/view/frontend/web/js/search-redirect.js
@@ -5,15 +5,8 @@ document.addEventListener("ffReady", function () {
         var redirectPath = '/FACT-Finder/result/';
 
         if (event.type === "search" && window.location.href.match(/FACT-Finder\/result/) === null) {
-            preventNativeSearch(event);
             var params = factfinder.common.dictToParameterString(event);
             window.location.href = domainUrl + redirectPath + params;
-            delete event.type;
         }
     });
-
-    function preventNativeSearch(event) {
-        delete event.type;
-        delete event.version;
-    }
 });
