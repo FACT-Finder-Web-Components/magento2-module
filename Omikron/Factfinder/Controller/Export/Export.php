99a62f7adbcb56b9a6b9a6634826c8a60c60e56f
a-laurowski <Panpiczek1!>
2019-03-11 09:27:59 +0100

    Remove Old product Export and Upload Helper

--- Omikron/Factfinder/Controller/Export/Export.php
+++ Omikron/Factfinder/Controller/Export/Export.php
@@ -5,18 +5,13 @@ namespace Omikron\Factfinder\Controller\Export;
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
 use Magento\Store\Model\Store;
-use Magento\Store\Model\StoreManagerInterface;
 use Omikron\Factfinder\Api\Config\ChannelProviderInterface;
 use Omikron\Factfinder\Model\Export\FeedFactory as FeedGeneratorFactory;
-use Omikron\Factfinder\Model\Export\Product as ProductExport;
 use Omikron\Factfinder\Model\StoreEmulation;
 use Omikron\Factfinder\Model\Stream\BrowserFactory;
 
 class Export extends Action
 {
-    /** @var ProductExport */
-    protected $productExport;
-
     /** @var ChannelProviderInterface */
     private $channelProvider;
 
@@ -34,14 +29,12 @@ class Export extends Action
 
     public function __construct(
         Context $context,
-        ProductExport $productExport,
         ChannelProviderInterface $channelProvider,
         StoreEmulation $storeEmulation,
         FeedGeneratorFactory $feedGeneratorFactory,
         BrowserFactory $browserFactory
     ) {
         parent::__construct($context);
-        $this->productExport        = $productExport;
         $this->channelProvider      = $channelProvider;
         $this->storeEmulation       = $storeEmulation;
         $this->feedGeneratorFactory = $feedGeneratorFactory;
