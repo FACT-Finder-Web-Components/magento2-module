2c6a17c5057490fcb2d138fbc99e36bc7f541146
Francesco Marangi <francesco.maragi@omikron.net>
2019-03-12 17:21:34 +0100

    Use core utilities for file downloads

--- Omikron/Factfinder/Controller/Export/Export.php
+++ Omikron/Factfinder/Controller/Export/Export.php
@@ -4,11 +4,12 @@ namespace Omikron\Factfinder\Controller\Export;
 
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
+use Magento\Framework\App\Response\Http\FileFactory;
 use Magento\Store\Model\Store;
 use Omikron\Factfinder\Api\Config\ChannelProviderInterface;
 use Omikron\Factfinder\Model\Export\FeedFactory as FeedGeneratorFactory;
 use Omikron\Factfinder\Model\StoreEmulation;
-use Omikron\Factfinder\Model\Stream\Browser;
+use Omikron\Factfinder\Model\Stream\CsvFactory;
 
 class Export extends Action
 {
@@ -21,6 +22,12 @@ class Export extends Action
     /** @var FeedGeneratorFactory */
     private $feedGeneratorFactory;
 
+    /** @var FileFactory */
+    private $fileFactory;
+
+    /** @var CsvFactory */
+    private $csvFactory;
+
     /** @var string */
     protected $feedType = 'product';
 
@@ -28,20 +35,26 @@ class Export extends Action
         Context $context,
         ChannelProviderInterface $channelProvider,
         StoreEmulation $storeEmulation,
-        FeedGeneratorFactory $feedGeneratorFactory
+        FeedGeneratorFactory $feedGeneratorFactory,
+        FileFactory $fileFactory,
+        CsvFactory $csvFactory
     ) {
         parent::__construct($context);
         $this->channelProvider      = $channelProvider;
         $this->storeEmulation       = $storeEmulation;
         $this->feedGeneratorFactory = $feedGeneratorFactory;
+        $this->csvFactory           = $csvFactory;
+        $this->fileFactory          = $fileFactory;
     }
 
     public function execute()
     {
         $storeId = (int) $this->getRequest()->getParam('store', Store::DEFAULT_STORE_ID);
         $this->storeEmulation->runInStore($storeId, function () {
-            $feedGenerator = $this->feedGeneratorFactory->create($this->feedType);
-            $feedGenerator->generate(new Browser("factfinder/export.{$this->channelProvider->getChannel()}.csv"));
+            $filename = "export.{$this->channelProvider->getChannel()}.csv";
+            $stream   = $this->csvFactory->create(['filename' => "factfinder/{$filename}"]);
+            $this->feedGeneratorFactory->create($this->feedType)->generate($stream);
+            $this->fileFactory->create($filename, $stream->getContent());
         });
     }
 }
