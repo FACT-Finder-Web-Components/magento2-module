6b6d43a0560aa72ef67341061daed74eb8f7f340
Francesco Marangi <2734054+fmarangi@users.noreply.github.com>
2019-04-10 09:47:28 +0200

    Exclude Web Components library from minification (#123)

--- src/Model/Export/Cms/Field/Image.php
+++ src/Model/Export/Cms/Field/Image.php
@@ -21,7 +21,7 @@ class Image implements PageFieldInterface
     public function getValue(PageInterface $page): string
     {
         $pattern = '#https?://[^/\s]+/\S+\.(jpe?g|png|gif)#';
-        preg_match($pattern, $this->filter->filter($page->getContent()), $result);
+        preg_match($pattern, $this->filter->filter((string) $page->getContent()), $result);
         return $result[0] ?? '';
     }
 }
--- src/view/frontend/requirejs-config.js
+++ src/view/frontend/requirejs-config.js
@@ -1,14 +1,14 @@
 var config = {
     map: {
         '*': {
-            factfinder: 'Omikron_Factfinder/ff-web-components/bundle.min'
+            factfinder: 'Omikron_Factfinder/ff-web-components/bundle'
         }
     },
     shim: {
-        'Omikron_Factfinder/ff-web-components/bundle.min': {
+        'Omikron_Factfinder/ff-web-components/bundle': {
             deps: [
-                'Omikron_Factfinder/ff-web-components/vendor/custom-elements-es5-adapter.min',
-                'Omikron_Factfinder/ff-web-components/vendor/webcomponents-loader.min'
+                'Omikron_Factfinder/ff-web-components/vendor/custom-elements-es5-adapter',
+                'Omikron_Factfinder/ff-web-components/vendor/webcomponents-loader'
             ],
             exports: 'factfinder'
         }
