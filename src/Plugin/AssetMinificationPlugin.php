5f17115a0bddb3bdbd5927f6bc190ccb7ffc2a08
Francesco Marangi <francesco.maragi@omikron.net>
2019-04-10 09:45:52 +0200

    Unit test

--- src/Plugin/AssetMinificationPlugin.php
+++ src/Plugin/AssetMinificationPlugin.php
@@ -6,8 +6,10 @@ namespace Omikron\Factfinder\Plugin;
 
 class AssetMinificationPlugin
 {
+    const JS_LIBRARY = '/Omikron_Factfinder/ff-web-components/';
+
     public function afterGetExcludes($_, array $result, string $contentType): array
     {
-        return array_merge($result, $contentType === 'js' ? ['/Omikron_Factfinder/ff-web-components/'] : []);
+        return array_merge($result, $contentType === 'js' ? [self::JS_LIBRARY] : []);
     }
 }
