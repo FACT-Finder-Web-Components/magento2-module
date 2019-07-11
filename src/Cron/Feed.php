5b181c4b396e75079875b36ea908d1026e1b7286
Francesco Marangi <francesco.marangi@omikron.net>
2019-07-11 10:27:57 +0200

    Formatting

--- src/Cron/Feed.php
+++ src/Cron/Feed.php
@@ -7,11 +7,11 @@ namespace Omikron\Factfinder\Cron;
 use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Store\Model\StoreManagerInterface;
 use Omikron\Factfinder\Api\Config\ChannelProviderInterface;
+use Omikron\Factfinder\Model\Api\PushImport;
 use Omikron\Factfinder\Model\Export\FeedFactory as FeedGeneratorFactory;
 use Omikron\Factfinder\Model\FtpUploader;
 use Omikron\Factfinder\Model\StoreEmulation;
 use Omikron\Factfinder\Model\Stream\CsvFactory;
-use Omikron\Factfinder\Model\Api\PushImport;
 
 class Feed
 {
@@ -41,7 +41,7 @@ class Feed
     /** @var string */
     private $feedType;
 
-    /** @var PushImport  */
+    /** @var PushImport */
     private $pushImport;
 
     public function __construct(
