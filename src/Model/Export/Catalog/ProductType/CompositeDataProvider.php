b4b30500d7c6ba3728419120276c8c2337b7c0d5
Aleksander Laurowski <aleksander.laurowski@omikron.net>
2019-07-15 09:10:42 +0200

    added composite provider for bundle and group products

--- src/Model/Export/Catalog/ProductType/CompositeDataProvider.php
+++ src/Model/Export/Catalog/ProductType/CompositeDataProvider.php
@@ -4,11 +4,35 @@ declare(strict_types=1);
 
 namespace Omikron\Factfinder\Model\Export\Catalog\ProductType;
 
+use Magento\Catalog\Model\Product;
+use Omikron\Factfinder\Model\Formatter\NumberFormatter;
+use Magento\Bundle\Model\Product\CatalogPrice;
+use Magento\Directory\Model\PriceCurrency;
+
 class CompositeDataProvider extends SimpleDataProvider
 {
+    /** @var CatalogPrice  */
+    private $priceModel;
+
+    /** @var PriceCurrency  */
+    private $priceCurrency;
+
+    public function __construct(
+        PriceCurrency $priceCurrency,
+        CatalogPrice $priceModel,
+        Product $product,
+        NumberFormatter $numberFormatter,
+        array $productFields = []
+    ){
+        parent::__construct($product, $numberFormatter, $productFields);
+        $this->priceModel = $priceModel;
+        $this->priceCurrency = $priceCurrency;
+    }
+
     public function toArray(): array
     {
-        $price = (float) $this->product->getPriceInfo()->getPrice('final_price')->getValue();
-        return ['Price' => $this->numberFormatter->format($price)] + parent::toArray();
+        return ['Price' => $this->numberFormatter->format(
+                $this->priceCurrency->convert($this->priceModel->getCatalogPrice($this->product)))
+            ] + parent::toArray();
     }
 }
