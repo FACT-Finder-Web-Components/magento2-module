d035bc53ac593fee49350a0fddb8be41c9b66633
Francesco Marangi <2734054+fmarangi@users.noreply.github.com>
2019-08-05 09:33:22 +0200

    FFWEB-1354: Cart campaigns and recommendations (#158)

---
+++ src/ViewModel/Cart.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Omikron\Factfinder\ViewModel;
+
+use Magento\Checkout\Model\Session;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\View\Element\Block\ArgumentInterface;
+use Magento\Quote\Model\Quote\Item as QuoteItem;
+
+class Cart implements ArgumentInterface
+{
+    /** @var Session */
+    private $checkoutSession;
+
+    public function __construct(Session $checkoutSession)
+    {
+        $this->checkoutSession = $checkoutSession;
+    }
+
+    /**
+     * @return QuoteItem[]
+     */
+    public function getItems(): array
+    {
+        try {
+            return $this->checkoutSession->getQuote()->getAllVisibleItems();
+        } catch (LocalizedException $e) {
+            return [];
+        }
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getItemIds(): array
+    {
+        return array_unique(array_map(function (QuoteItem $quoteItem): string {
+            return (string) $quoteItem->getProduct()->getData('sku');
+        }, $this->getItems()));
+    }
+}
