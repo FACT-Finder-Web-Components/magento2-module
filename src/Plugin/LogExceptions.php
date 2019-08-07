22ef7a1cf12cc654a0cc8cd000afb039c76d32ca
Francesco Marangi <francesco.marangi@omikron.net>
2019-08-07 10:49:19 +0200

    Fix unit test

--- src/Plugin/LogExceptions.php
+++ src/Plugin/LogExceptions.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Omikron\Factfinder\Plugin;
 
 use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\Phrase;
 use Omikron\Factfinder\Exception\ResponseException;
 use Omikron\Factfinder\Model\Api\PushImport;
 use Psr\Log\LoggerInterface;
@@ -37,10 +38,9 @@ class LogExceptions
             return $proceed(...$params);
         } catch (ResponseException $e) {
             if ($this->scopeConfig->isSetFlag('factfinder/general/logging_enabled')) {
-                $this->logger->error(__(
+                $this->logger->error(new Phrase(
                     'FACT-Finder response exception: %1, thrown at %2',
-                    $e->getMessage(),
-                    $e->getTraceAsString()
+                    [$e->getMessage(), $e->getTraceAsString()]
                 ));
             }
             return false;
