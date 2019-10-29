883fe873d46ffce8d101907d1f42eebd4412d7ba
Francesco Marangi <francesco.marangi@omikron.net>
2019-10-29 10:07:36 +0100

    Update README

--- src/view/frontend/web/js/search-navigation.js
+++ src/view/frontend/web/js/search-navigation.js
@@ -2,11 +2,15 @@ define(['factfinder', 'mage/url'], function (factfinder, url) {
     var redirectPath = 'FACT-Finder/result';
 
     factfinder.communication.FFCommunicationEventAggregator.addBeforeDispatchingCallback(function (event) {
-        if (event.type === 'search' && !event.__immediate) {
+        if (event.type === 'search' && !isSearchResultPage() && !event.__immediate) {
             delete event.type;
             var params = factfinder.common.dictToParameterString(event);
             if (!url.build('')) url.setBaseUrl(BASE_URL || '');
             window.location = url.build(redirectPath + params);
         }
     });
+
+    function isSearchResultPage() {
+        return window.location.href.indexOf(redirectPath) >= 0;
+    }
 });
