96c70280bc66f0e999a85534e72731e694d6e8c3
Aleksander Laurowski <aleksander.laurowski@omikron.net>
2019-10-31 12:49:39 +0100

    FFWEB-1411: Added loggedIn flag communication customer data after customer is logged in to prevent continuously login tracking on after each reload

--- src/view/frontend/web/js/view/ffcommunication.js
+++ src/view/frontend/web/js/view/ffcommunication.js
@@ -9,16 +9,23 @@ define([
         /** @inheritdoc */
         initialize: function () {
             this._super();
-            customerData.reload(['ffcommunication']).done(function (result) {
-                var communication = $('ff-communication'),
-                    uid = result.ffcommunication.uid,
-                    sid = result.ffcommunication.sid;
+            var cacheKey               = 'ffcommunication',
+                communicationData      = customerData.get('ffcommunication')(),
+                communicationComponent = $('ff-communication');
 
-                communication.attr('sid', sid);
-                if (!!uid) {
-                    communication.attr('user-id', uid);
-                }
-            });
+            if (!communicationData.loggedIn && !communicationComponent.attr('user-id')) {
+                customerData.reload([cacheKey]).done(function (result) {
+                    var communicationComponent = $('ff-communication'),
+                        uid = result.ffcommunication.uid,
+                        sid = result.ffcommunication.sid;
+
+                    communicationComponent.attr('sid', sid);
+                    if (!!uid) {
+                        communicationComponent.attr('user-id', uid);
+                        customerData.set(cacheKey, Object.assign({loggedIn: true}, result.ffcommunication));
+                    }
+                });
+            }
         }
     });
 });
