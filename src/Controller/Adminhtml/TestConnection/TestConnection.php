c53449d1a906466d790d2bd1d155ea1fbc599a11
Francesco Marangi <2734054+fmarangi@users.noreply.github.com>
2019-11-05 11:00:54 +0100

    Prevent errors during connection test when fields are disabled (#192)

--- src/Controller/Adminhtml/TestConnection/TestConnection.php
+++ src/Controller/Adminhtml/TestConnection/TestConnection.php
@@ -6,7 +6,10 @@ namespace Omikron\Factfinder\Controller\Adminhtml\TestConnection;
 
 use Magento\Backend\App\Action;
 use Magento\Framework\Controller\Result\JsonFactory;
+use Magento\Framework\Phrase;
 use Omikron\Factfinder\Api\Config\AuthConfigInterface;
+use Omikron\Factfinder\Api\Config\CommunicationConfigInterface;
+use Omikron\Factfinder\Exception\ResponseException;
 use Omikron\Factfinder\Model\Api\CredentialsFactory;
 use Omikron\Factfinder\Model\Api\TestConnection as ApiConnectionTest;
 
@@ -27,29 +30,35 @@ class TestConnection extends Action
     /** @var AuthConfigInterface */
     private $authConfig;
 
+    /** @var CommunicationConfigInterface */
+    private $communicationConfig;
+
     public function __construct(
         Action\Context $context,
         JsonFactory $jsonResultFactory,
         CredentialsFactory $credentialsFactory,
         AuthConfigInterface $authConfig,
+        CommunicationConfigInterface $communicationConfig,
         ApiConnectionTest $testConnection
     ) {
         parent::__construct($context);
-        $this->jsonResultFactory  = $jsonResultFactory;
-        $this->credentialsFactory = $credentialsFactory;
-        $this->testConnection     = $testConnection;
-        $this->authConfig         = $authConfig;
+        $this->jsonResultFactory   = $jsonResultFactory;
+        $this->credentialsFactory  = $credentialsFactory;
+        $this->testConnection      = $testConnection;
+        $this->authConfig          = $authConfig;
+        $this->communicationConfig = $communicationConfig;
     }
 
     public function execute()
     {
-        $message = __('Connection successfully established.');
+        $message = new Phrase('Connection successfully established.');
 
         try {
-            $request = $this->getRequest();
-            $params  = $this->getCredentials($request->getParams()) + ['channel' => $request->getParam('channel')];
-            $this->testConnection->execute($request->getParam('address'), $params);
-        } catch (\Exception $e) {
+            $request   = $this->getRequest();
+            $params    = $this->getCredentials($request->getParams()) + ['channel' => $request->getParam('channel')];
+            $serverUrl = $request->getParam('address', $this->communicationConfig->getAddress());
+            $this->testConnection->execute($serverUrl, $params);
+        } catch (ResponseException $e) {
             $message = $e->getMessage();
         }
 
@@ -59,13 +68,13 @@ class TestConnection extends Action
     private function getCredentials(array $params): array
     {
         // The password wasn't edited, load it from config
-        if ($params['password'] === $this->obscuredValue) {
+        if (!isset($params['password']) || $params['password'] === $this->obscuredValue) {
             $params['password'] = $this->authConfig->getPassword();
         }
 
         $params += [
-            'prefix'  => $params['authentication_prefix'],
-            'postfix' => $params['authentication_postfix'],
+            'prefix'  => $params['authentication_prefix'] ?? $this->authConfig->getAuthenticationPrefix(),
+            'postfix' => $params['authentication_postfix'] ?? $this->authConfig->getAuthenticationPostfix(),
         ];
         return $this->credentialsFactory->create($params)->toArray();
     }
