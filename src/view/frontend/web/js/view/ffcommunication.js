b3119bfa1a16e38c2f0bb8a17d5b2d347287efe5
Francesco Marangi <francesco.marangi@omikron.net>
2019-11-07 09:56:31 +0100

    Upgrade Web Components to version 3.8.0

--- src/view/frontend/web/js/view/ffcommunication.js
+++ src/view/frontend/web/js/view/ffcommunication.js
@@ -4,9 +4,8 @@ define(['Magento_Customer/js/customer-data'], function (customerData) {
     return function (config, element) {
         var sessionData = customerData.get('ffcommunication');
         sessionData.subscribe(function (data) {
-            if (!data.uid) return;
-            element.sid = data.sid;
-            element.userId = data.uid;
+            if (data.sid) element.sid = data.sid;
+            if (data.uid) element.userId = data.uid;
         });
         sessionData.valueHasMutated();
     };
