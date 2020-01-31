da1a046f892414b673f2a092e5c0018e0883ec8d
Francesco Marangi <francesco.marangi@omikron.net>
2020-01-31 13:28:12 +0100

    Server-side rendering PoC

---
+++ src/Block/Ssr.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Omikron\Factfinder\Block;
+
+use Magento\Framework\View\Element\Template;
+use Omikron\Factfinder\Model\Ssr\SearchAdapter;
+
+class Ssr extends Template
+{
+    /** @var SearchAdapter */
+    private $searchAdapter;
+
+    public function __construct(Template\Context $context, SearchAdapter $searchAdapter, array $data = [])
+    {
+        parent::__construct($context, $data);
+        $this->searchAdapter = $searchAdapter;
+    }
+
+    /**
+     * @return string
+     * @SuppressWarnings(PHPMD.CamelCaseMethodName)
+     */
+    protected function _toHtml()
+    {
+        $output = $this->getChildHtml();
+        $output = preg_replace_callback('#<ff-record-list([^>]*?)>#s', function (array $match) {
+            $attributes = preg_replace('#\sunresolved\s?#s', '', $match[1]);
+            return "<ff-record-list ssr-rendered{$attributes}>";
+        }, $output);
+
+        return preg_replace_callback('#<ff-record[\s>].*?</ff-record>#s', function (array $match): string {
+            return $this->preRendered($match[0]) . '<template data-role="record">' . $match[0] . '</template>';
+        }, $output);
+    }
+
+    public function preRendered(string $template): string
+    {
+        $records = $this->searchAdapter->search($this->getRequest()->getParam('query', '*'), $this->getSearchParams());
+        return implode('', array_map($this->renderRecord($template), $records));
+    }
+
+    private function renderRecord(string $template): callable
+    {
+        return function (array $record) use ($template): string {
+            $this->assign($record);
+            $templateEngine = $this->templateEnginePool->get('mustache');
+            return $templateEngine->render($this->templateContext, $template, $this->_viewVars);
+        };
+    }
+
+    private function getSearchParams(): array
+    {
+        $params = explode(',', (string) $this->getData('search_params'));
+        return array_reduce($params, function (array $result, string $part): array {
+            [$key, $value] = array_map('urldecode', explode('=', $part));
+            return $result + [$key => $value];
+        }, []);
+    }
+}
---
+++ src/Test/Unit/Model/Ssr/Template/FilterTest.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Omikron\Factfinder\Model\Ssr\Template;
+
+use Omikron\Factfinder\Api\FieldRolesInterface;
+use PHPUnit\Framework\TestCase;
+
+class FilterTest extends TestCase
+{
+    /** @var Filter */
+    private $filter;
+
+    public function test_anchor_links_are_replaced()
+    {
+        $from = '<a class="product-image-container" data-anchor="{{record.ProductURL}}">';
+        $to   = '<a class="product-image-container" href="{{record.ProductURL}}" data-anchor="{{record.ProductURL}}">';
+        $this->assertSame($this->filter->filterValue($from), $to);
+    }
+
+    public function test_data_targets_are_replaced()
+    {
+        $from = '<a class="product-image-container" data-redirect-target="_blank">';
+        $to   = '<a class="product-image-container" target="_blank" data-redirect-target="_blank">';
+        $this->assertSame($this->filter->filterValue($from), $to);
+    }
+
+    public function test_image_src_are_replaced()
+    {
+        $from = '<img class="product-image-photo" data-image="{{record.ImageUrl}}" alt="{{record.Name}}" />';
+        $to   = '<img class="product-image-photo" src="{{record.ImageUrl}}" data-image="{{record.ImageUrl}}" alt="{{record.Name}}" />';
+        $this->assertSame($this->filter->filterValue($from), $to);
+
+        $from = '<img class="product-image-photo" data-image alt="{{record.Name}}" />';
+        $to   = '<img class="product-image-photo" src="{{record.CustomUrl}}" data-image alt="{{record.Name}}" />';
+        $this->assertSame($this->filter->filterValue($from), $to);
+    }
+
+    protected function setUp()
+    {
+        $fieldRoles = $this->createMock(FieldRolesInterface::class);
+        $fieldRoles->method('getFieldRole')->with('imageUrl', $this->anything())->willReturn('CustomUrl');
+        $this->filter = new Filter($fieldRoles);
+    }
+}
