9637903816b7b0bb475f284566fc4119a7ed2379
Aleksander Laurowski <aleksander.laurowski@omikron.net>
2020-03-27 12:21:49 +0100

    FFWEB-1617: Upgrade Web Components to 3.13.0

--- src/view/frontend/web/js/search-navigation.js
+++ src/view/frontend/web/js/search-navigation.js
@@ -2,8 +2,8 @@ define(['factfinder', 'mage/url'], function (factfinder, url) {
     var redirectPath = 'FACT-Finder/result';
 
     factfinder.communication.EventAggregator.addBeforeDispatchingCallback(function (event) {
-        if (event.type === 'search' && !isSearchResultPage() && !event.__immediate && event.navigation !== 'true') {
-            delete event.type;
+        if (event.type === 'search' && !isSearchResultPage() && !event.searchImmediate && event.navigation !== 'true') {
+            event.cancel();
             var params = factfinder.common.dictToParameterString(event);
             if (!url.build('')) url.setBaseUrl(BASE_URL || '');
             window.location = url.build(redirectPath + params);
