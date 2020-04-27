2a589b66a12eb092e9bfec8a80df8d2aa62fede2
Aleksander Laurowski <aleksander.laurowski@omikron.net>
2020-04-27 16:23:07 +0200

    update CHANGELOG.md

--- src/view/frontend/web/js/search-navigation.js
+++ src/view/frontend/web/js/search-navigation.js
@@ -2,7 +2,7 @@ define(['factfinder', 'mage/url'], function (factfinder, url) {
     var redirectPath = 'FACT-Finder/result';
 
     factfinder.communication.EventAggregator.addBeforeDispatchingCallback(function (event) {
-        if (event.type === 'search' && !isSearchResultPage() && !event.searchImmediate && event.navigation !== 'true') {
+        if (event.type === 'search' && !isSearchResultPage() && !event.searchImmediate && event.navigation !== 'true' && !isLoadingPreviousPages()) {
             event.cancel();
             var params = factfinder.common.dictToParameterString(event);
             if (!url.build('')) url.setBaseUrl(BASE_URL || '');
@@ -12,5 +12,9 @@ define(['factfinder', 'mage/url'], function (factfinder, url) {
 
     function isSearchResultPage() {
         return window.location.href.indexOf(redirectPath) >= 0;
+    }
+
+    function isLoadingPreviousPages() {
+        return event.productsPerPage >= factfinder.communication.EventAggregator.currentSearchResult.paging.resultsPerPage;
     }
 });
