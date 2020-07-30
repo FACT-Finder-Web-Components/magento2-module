14dbc7948ea2f380f8c50d275fc7130a54f10d25
Francesco Marangi <2734054+fmarangi@users.noreply.github.com>
2020-07-30 16:52:58 +0200

    Fix all GET params containing spaces

--- src/Model/Http/ParameterUtils.php
+++ src/Model/Http/ParameterUtils.php
@@ -18,7 +18,7 @@ class ParameterUtils
         return array_combine(array_map(function (string $key): string {
             // changing + to spaces to be compatible with PHP_QUERY_RFC1738
             // @link https://www.php.net/manual/en/function.http-build-query.php
-            return preg_match('#^filter(.*?)ROOT/#', $key) ? str_replace('_', ' ', $key) : $key;
+            return preg_match('#^filter#', $key) ? str_replace('_', ' ', $key) : $key;
         }, array_keys($params)), array_values($params));
     }
 }
