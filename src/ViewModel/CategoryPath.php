284f3376cc81ce5301ea25caadec832f2f9531da
Francesco Marangi <2734054+fmarangi@users.noreply.github.com>
2020-10-13 09:00:43 +0200

    Make category optional in CategoryPath view model

--- src/ViewModel/CategoryPath.php
+++ src/ViewModel/CategoryPath.php
@@ -41,20 +41,20 @@ class CategoryPath implements ArgumentInterface
     }
 
     /**
-     * @param Category $category
+     * @param Category|null $category
      *
      * @return Category[]
      */
-    private function getParentCategories(Category $category): array
+    private function getParentCategories(?Category $category): array
     {
-        $categories = $category->getParentCategories();
+        $categories = $category ? $category->getParentCategories() : [];
         usort($categories, function (Category $a, Category $b): int {
             return $a->getLevel() - $b->getLevel();
         });
         return $categories;
     }
 
-    private function getCurrentCategory(): Category
+    private function getCurrentCategory(): ?Category
     {
         return $this->registry->registry('current_category');
     }
