f47c60cd232d85572adc1c6404d472d8de8d33d3
Aleksander Laurowski <aleksander.laurowski@omikron.net>
2020-12-01 12:39:35 +0100

    reworked checkout tracking

--- src/Test/Unit/Observer/CheckoutTest.php
+++ src/Test/Unit/Observer/CheckoutTest.php
@@ -9,24 +9,24 @@ use Magento\Framework\Event\Observer;
 use Magento\Quote\Model\Quote;
 use Magento\Quote\Model\Quote\Item;
 use Omikron\Factfinder\Api\Config\CommunicationConfigInterface;
-use Omikron\Factfinder\Api\Data\TrackingProductInterface;
-use Omikron\Factfinder\Api\Data\TrackingProductInterfaceFactory;
 use Omikron\Factfinder\Api\FieldRolesInterface;
-use Omikron\Factfinder\Model\Api\Action\Standard\Tracking;
-use Omikron\Factfinder\Model\Api\ActionFactory;
+use Omikron\FactFinder\Communication\Credentials;
+use Omikron\FactFinder\Communication\Resource\Builder;
+use Omikron\FactFinder\Communication\Resource\Tracking\Product as TrackingProduct;
+use Omikron\FactFinder\Communication\ResourceInterface;
+use Omikron\Factfinder\Model\Api\CredentialsFactory;
+use Omikron\Factfinder\Model\SessionData;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
+use Psr\Log\LoggerInterface;
 
 class CheckoutTest extends TestCase
 {
-    /** @var MockObject|Tracking */
-    private $trackingMock;
+    /** @var MockObject|Builder */
+    private $builderMock;
 
-    /** @var MockObject|ActionFactory */
-    private $actionFactoryMock;
-
-    /** @var MockObject|TrackingProductInterfaceFactory */
-    private $trackingProductFactoryMock;
+    /** @var MockObject|ResourceInterface */
+    private $resourceMock;
 
     /** @var MockObject|CommunicationConfigInterface */
     private $configMock;
@@ -36,20 +36,29 @@ class CheckoutTest extends TestCase
 
     public function test_execute_should_call_tracking_with_parameters_of_correct_type()
     {
+        $qty                   = 1;
+        $price                 = 9.99;
+        $trackingProductNumber = '1';
+        $sku                   = 'product-sku-1';
+
         $this->configMock->method('isChannelEnabled')->willReturn(true);
+        $productMock = $this->createConfiguredMock(Product::class, ['getId' => $trackingProductNumber, 'getSku' => $sku]);
+        $quoteMock   = $this->createConfiguredMock(Quote::class, [
+            'getAllVisibleItems' => [$this->createConfiguredMock(Item::class, ['getPrice' => $price, 'getQty' => $qty, 'getProduct' => $productMock])]
+        ]);
 
-        $quoteMock = $this->createConfiguredMock(Quote::class, ['getAllVisibleItems' => [
-            $this->createConfiguredMock(Item::class, ['getProduct' => $this->createMock(Product::class)]),
-            $this->createConfiguredMock(Item::class, ['getProduct' => $this->createMock(Product::class)]),
-        ]]);
+        $this->fieldRolesMock->expects($this->exactly(2))->method('fieldRoleToAttribute')->willReturnMap(
+            [
+                [$productMock, 'trackingProductNumber', $trackingProductNumber],
+                [$productMock, 'masterArticleNumber', $sku],
+            ]);
 
-        $this->trackingProductFactoryMock->expects($this->atLeastOnce())
-            ->method('create')
-            ->willReturn($this->getMockBuilder(TrackingProductInterface::class)->getMock());
+        $expectedProducts = [
+            new TrackingProduct($trackingProductNumber, $sku, $price, $qty)
+        ];
 
-        $this->trackingMock->expects($this->once())
-            ->method('execute')
-            ->with($this->isType('string'), $this->containsOnlyInstancesOf(TrackingProductInterface::class));
+        $this->resourceMock->expects($this->once())->method('track')
+            ->with($this->anything(), $this->anything(), $expectedProducts, $this->anything());
 
         $this->checkoutObserver->execute(new Observer(['quote' => $quoteMock]));
     }
@@ -57,26 +66,37 @@ class CheckoutTest extends TestCase
     public function test_tracking_is_skipped_if_the_integration_is_disabled()
     {
         $this->configMock->method('isChannelEnabled')->willReturn(false);
-        $this->trackingMock->expects($this->never())->method('execute');
-        $this->checkoutObserver->execute(new Observer(['quote' => $this->createMock(Quote::class)]));
+        $observerMock = $this->createMock(Observer::class);
+        $observerMock->expects($this->never())->method('getData');
+        $this->checkoutObserver->execute($observerMock);
     }
 
     protected function setUp(): void
     {
-        $this->trackingMock = $this->createMock(Tracking::class);
-        $this->configMock = $this->createMock(CommunicationConfigInterface::class);
-        $this->trackingProductFactoryMock = $this->getMockBuilder(TrackingProductInterfaceFactory::class)
-            ->disableOriginalConstructor()
-            ->setMethods(['create'])
-            ->getMock();
+        $this->configMock     = $this->createMock(CommunicationConfigInterface::class);
+        $this->resourceMock   = $this->createConfiguredMock(ResourceInterface::class, ['track' => []]);
+        $this->builderMock    = $this->createMock(Builder::class);
+        $this->fieldRolesMock = $this->createMock(FieldRolesInterface::class);
+        $this->resourceMock   = $this->createConfiguredMock(ResourceInterface::class, ['track' => []]);
+        $this->builderMock->method('withApiVersion')->willReturn($this->builderMock);
+        $this->builderMock->method('withServerUrl')->willReturn($this->builderMock);
+        $this->builderMock->method('withCredentials')->willReturn($this->builderMock);
+        $this->builderMock->method('withLogger')->willReturn($this->builderMock);
+        $this->builderMock->method('build')->willReturn($this->resourceMock);
 
-        $this->actionFactoryMock = $this->createConfiguredMock(ActionFactory::class, ['getTracking' => $this->trackingMock]);
-        $this->actionFactoryMock->method('withApiVersion')->with($this->anything())->willReturn($this->actionFactoryMock);
+        $this->fieldRolesMock->method('getFieldRole')->willReturnMap(
+            [
+                ['trackingProductNumber', null, 'id'],
+                ['masterArticleNumber', null, 'sku'],
+            ]);
+
         $this->checkoutObserver = new Checkout(
-            $this->actionFactoryMock,
-            $this->trackingProductFactoryMock,
-            $this->createMock(FieldRolesInterface::class),
-            $this->configMock
+            $this->fieldRolesMock,
+            $this->configMock,
+            $this->createConfiguredMock(CredentialsFactory::class, ['create' => $this->createMock(Credentials::class)]),
+            $this->createMock(SessionData::class),
+            $this->builderMock,
+            $this->createMock(LoggerInterface::class)
         );
     }
 }
