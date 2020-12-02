274e9be0d41e0c079c79e15eea950b4721e52231
Aleksander Laurowski <aleksander.laurowski@omikron.net>
2020-12-02 11:07:40 +0100

    reworked integration test

--- src/Test/Integration/Controller/ProxyCallTest.php
+++ src/Test/Integration/Controller/ProxyCallTest.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 namespace Omikron\Factfinder\Test\Integration\Controller;
 
 use Magento\TestFramework\TestCase\AbstractController;
-use Omikron\Factfinder\Api\ClientInterface;
-use Omikron\Factfinder\Model\Api\ClientFactory;
+use Omikron\FactFinder\Communication\ClientInterface;
+use Omikron\FactFinder\Communication\Resource\Builder;
 use PHPUnit\Framework\MockObject\MockObject;
 
 /**
@@ -14,13 +14,16 @@ use PHPUnit\Framework\MockObject\MockObject;
  */
 class ProxyCallTest extends AbstractController
 {
+    /** @var MockObject|Builder */
+    private $builderMock;
+
     /** @var MockObject|ClientInterface */
-    private $apiClient;
+    private $clientMock;
 
     public function test_JSON_endpoints_are_accepted_by_the_proxy_controller()
     {
-        $this->apiClient->expects($this->atLeastOnce())
-            ->method('get')
+        $this->clientMock->expects($this->atLeastOnce())
+            ->method('getRequest')
             ->with($this->stringContains('/Suggest.ff'), $this->anything());
 
         $this->dispatch('/FACT-Finder/Suggest.ff?query=asd');
@@ -29,8 +32,8 @@ class ProxyCallTest extends AbstractController
 
     public function test_rest_calls_are_accepted_by_the_proxy_controller()
     {
-        $this->apiClient->expects($this->atLeastOnce())
-            ->method('get')
+        $this->clientMock->expects($this->atLeastOnce())
+            ->method('getRequest')
             ->with($this->stringContains('/rest/v1/records/'), $this->anything());
 
         $this->dispatch('/FACT-Finder/rest/v1/records/my_channel?sid=abc');
@@ -39,15 +42,15 @@ class ProxyCallTest extends AbstractController
 
     public function test_other_request_paths_are_ignored()
     {
-        $this->apiClient->expects($this->never())->method('get');
+        $this->clientMock->expects($this->never())->method('getRequest');
         $this->dispatch('/FACT-Finder/non-existing-endpoint');
         $this->assert404NotFound();
     }
 
     public function test_filter_parameters_are_correctly_encoded()
     {
-        $this->apiClient->expects($this->atLeastOnce())
-            ->method('get')
+        $this->clientMock->expects($this->atLeastOnce())
+            ->method('getRequest')
             ->with($this->anything(), ['filterCategoryPathROOT' => 'First Category', 'filterCategoryPathROOT/First Category' => 'Second Category']);
 
         $this->dispatch('/FACT-Finder/Search.ff?filterCategoryPathROOT=First+Category&filterCategoryPathROOT%2FFirst+Category=Second+Category');
@@ -55,8 +58,8 @@ class ProxyCallTest extends AbstractController
 
     public function test_filter_parameters_are_correctly_encoded_2()
     {
-        $this->apiClient->expects($this->atLeastOnce())
-            ->method('get')
+        $this->clientMock->expects($this->atLeastOnce())
+            ->method('getRequest')
             ->with($this->anything(), ['filterabgerundete Ecken vorhanden' => 'Ja', 'query' => 'pro']);
 
         $this->dispatch('/FACT-Finder/Search.ff?query=pro&filterabgerundete+Ecken+vorhanden=Ja');
@@ -65,14 +68,20 @@ class ProxyCallTest extends AbstractController
     protected function setUp(): void
     {
         parent::setUp();
-        $this->apiClient = $this->createMock(ClientInterface::class);
-        $this->clientFactory = $this->createConfiguredMock(ClientFactory::class, ['create' => $this->apiClient]);
-        $this->_objectManager->addSharedInstance($this->clientFactory, ClientFactory::class);
+        $this->clientMock  = $this->createConfiguredMock(ClientInterface::class, ['getRequest' => []]);
+        $this->builderMock = $this->createMock(Builder::class);
+        $this->builderMock->method('withApiVersion')->willReturn($this->builderMock);
+        $this->builderMock->method('withServerUrl')->willReturn($this->builderMock);
+        $this->builderMock->method('withCredentials')->willReturn($this->builderMock);
+        $this->builderMock->method('withLogger')->willReturn($this->builderMock);
+        $this->builderMock->method('client')->willReturn($this->clientMock);
+
+        $this->_objectManager->addSharedInstance($this->builderMock, Builder::class);
     }
 
     protected function tearDown(): void
     {
-        $this->_objectManager->removeSharedInstance(ClientFactory::class);
+        $this->_objectManager->removeSharedInstance(Builder::class);
         parent::tearDown();
     }
 }
