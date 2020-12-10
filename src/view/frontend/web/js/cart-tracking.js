118475d079b17e883dfbd761a25ffcc5070a1097
Aleksander Laurowski <aleksander.laurowski@omikron.net>
2020-12-10 07:59:27 +0100

    add frontend checkout tracking, refactor file names

---
+++ src/view/frontend/web/js/cart-tracking.js
@@ -0,0 +1,45 @@
+define(['factfinder', 'jquery', 'Magento_Customer/js/customer-data'], function (factfinder, jQuery, customerData) {
+    'use strict';
+
+    return function (options, element) {
+        jQuery(document).on('ajax:addToCart', function (_, eventData) {
+            const cart = customerData.get('cart');
+            let payload, subscription;
+            payload = eventData
+
+            if (!subscription || subscription.isDisposed) {
+                subscription = cart.subscribe(function (cartData) {
+                    if (payload.productInfo && payload.productInfo.length) {
+                        const addedProductId = payload.productInfo[0].id;
+                        const cartItem = unpackOrUndefined(cartData.items.filter(function (item) {
+                            return item.product_id === addedProductId;
+                        }));
+                        const qtyInput = unpackOrUndefined(jQuery(payload.form[0]).serializeArray().filter(function (element) {
+                            return element.name === 'qty';
+                        }));
+
+                        if (qtyInput && cartItem) {
+                            const track = new factfinder.communication.Tracking12();
+                            track.cart({
+                                channel: factfinder.communication.globalSearchParameter.channel,
+                                sid: factfinder.common.localStorage.getItem('ff_sid'),
+                                id: cartItem.product_sku,
+                                price: cartItem.product_price_value,
+                                masterId: payload.sku,
+                                count: parseInt(qtyInput.value)
+                            });
+                        }
+                    }
+                    subscription.dispose();
+                });
+            }
+        });
+
+        function unpackOrUndefined(arrayElement) {
+            if (Array.isArray(arrayElement) && arrayElement.length) {
+                return arrayElement[0];
+            }
+            return undefined;
+        }
+    }
+});
