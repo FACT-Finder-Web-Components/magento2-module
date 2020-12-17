792a4f23c183e3147669fd1ddcd144cf66698551
a.laurowski <ext-a.laurowski@omikron.net>
2020-12-17 14:38:25 +0100

    FFWEB-1889: Rework tracking

---
+++ src/ViewModel/Order.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Omikron\Factfinder\ViewModel;
+
+use Magento\Checkout\Model\Session;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\View\Element\Block\ArgumentInterface;
+use Magento\Sales\Model\Order\Item as OrderItem;
+use Omikron\Factfinder\Api\Config\CommunicationConfigInterface;
+
+class Order implements ArgumentInterface
+{
+    /** @var Session */
+    private $checkoutSession;
+
+    /** @var CommunicationConfigInterface */
+    private $communicationConfig;
+
+    public function __construct(Session $checkoutSession, CommunicationConfigInterface $communicationConfig)
+    {
+        $this->checkoutSession     = $checkoutSession;
+        $this->communicationConfig = $communicationConfig;
+    }
+
+    /**
+     * @return OrderItem[]
+     */
+    public function getItems(): array
+    {
+        try {
+            return $this->checkoutSession->getLastRealOrder()->getAllVisibleItems();
+        } catch (LocalizedException $e) {
+            return [];
+        }
+    }
+
+    public function getChannel(): string
+    {
+        return $this->communicationConfig->getChannel();
+    }
+}
