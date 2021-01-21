719ffe06a75143ba33f90ce21802cb7a9b38330e
Aleksander Laurowski <aleksander.laurowski@omikron.net>
2021-01-21 11:36:03 +0100

    fixup! FFWEB-1763 rework communication

--- src/Plugin/LogExceptions.php
+++ src/Plugin/LogExceptions.php
@@ -6,7 +6,7 @@ namespace Omikron\Factfinder\Plugin;
 
 use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\Phrase;
-use Omikron\Factfinder\Exception\ResponseException;
+use Omikron\FactFinder\Communication\Client\ClientException;
 use Omikron\Factfinder\Model\Api\PushImport;
 use Psr\Log\LoggerInterface;
 
@@ -36,7 +36,7 @@ class LogExceptions
     {
         try {
             return $proceed(...$params);
-        } catch (ResponseException $e) {
+        } catch (ClientException $e) {
             if ($this->scopeConfig->isSetFlag('factfinder/general/logging_enabled')) {
                 $this->logger->error(new Phrase(
                     'FACT-Finder response exception: %1, thrown at %2',
