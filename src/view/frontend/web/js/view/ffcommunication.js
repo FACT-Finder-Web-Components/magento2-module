18e4d042d5f0241b55f59ef78c60a785662e2df8
Aleksander Laurowski <aleksander.laurowski@omikron.net>
2021-05-05 15:51:51 +0200

    FFWEB-1892 prevent sending redundant login tracking requests

--- src/view/frontend/web/js/view/ffcommunication.js
+++ src/view/frontend/web/js/view/ffcommunication.js
@@ -1,10 +1,21 @@
-define(['Magento_Customer/js/customer-data'], function (customerData) {
+define(['Magento_Customer/js/customer-data', 'factfinder'], function (customerData, factfinder) {
     'use strict';
 
     return function (config, element) {
         var sessionData = customerData.get('ffcommunication');
         sessionData.subscribe(function (data) {
-            if (data.uid && data.uid !== element.userId) element.userId = data.uid;
+            if (!data.uid) {
+                const uidKey = Object.keys(localStorage).find(function (key) {
+                    return key.indexOf(factfinder.common.localStorage.getItem('ff_sid')) === 0
+                });
+                factfinder.common.localStorage.setItem(uidKey, null);
+            }
+
+            const storedUserId = factfinder.common.localStorage.getItem(factfinder.common.localStorage.getItem('ff_sid') + ':' + data.uid);
+            if (data.uid && data.uid !== element.userId && (!storedUserId || storedUserId !== data.uid.toString())) {
+                element.userId = data.uid;
+            }
+
             if (data.internal) element.addParams = (element.addParams ? element.addParams + ',' : '') + 'log=internal';
         });
         sessionData.valueHasMutated();
