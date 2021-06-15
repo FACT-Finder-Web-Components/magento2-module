75a8b8c16e36f5ece88f3bb9ef31cec0bce62642
paulfcdd <Ctrnjh56ufpf!>
2021-06-15 14:57:46 +0200

    FFWEB-2062: Update tests

--- src/Test/Unit/Model/Export/Catalog/AttributeValuesExtractorTest.php
+++ src/Test/Unit/Model/Export/Catalog/AttributeValuesExtractorTest.php
@@ -7,20 +7,42 @@ use Magento\Email\Model\Template\Filter;
 use Omikron\Factfinder\Api\Filter\FilterInterface;
 use Magento\Catalog\Model\Product;
 use Magento\Catalog\Model\ResourceModel\Eav\Attribute;
+use Omikron\Factfinder\Model\Filter\TextFilter;
 use Omikron\Factfinder\Model\Formatter\NumberFormatter;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
 class AttributeValuesExtractorTest extends TestCase
 {
-    private $filterMock;
+    /** @var NumberFormatter|MockObject  */
     private $numberFormatter;
+
+    /** @var AttributeValuesExtractor  */
     private $attributeExtractor;
 
-    public function test_it_return_empty_string_on_null_value()
+    public function test_it_returns_scalar_value()
     {
-        $productMock = $this->createMock(Product::class, ['getSku' => 'sku-1']);
-        $attributeMock = $this->createMock(Attribute::class, []);
+        $attributeValue  = 'Value';
+        $productMock = $this->createConfiguredMock(Product::class, [
+            'getDataUsingMethod' => $attributeValue
+        ]);
+        $attributeMock = $this->createConfiguredMock(Attribute::class, [
+            'getAttributeCode' => 'test-atttribute',
+            'getFrontendInput' => 'varchar'
+        ]);
+        $attributeValues = $this->attributeExtractor->getAttributeValues($productMock, $attributeMock);
+        $this->assertEquals([$attributeValue], $attributeValues);
+    }
+
+    public function test_it_returns_empty_string_on_null_value()
+    {
+        $productMock = $this->createConfiguredMock(Product::class, [
+            'getDataUsingMethod' => null
+        ]);
+        $attributeMock   = $this->createConfiguredMock(Attribute::class, [
+            'getAttributeCode' => 'test-atttribute',
+            'getFrontendInput' => 'varchar'
+        ]);
         $attributeValues = $this->attributeExtractor->getAttributeValues($productMock, $attributeMock);
         $this->assertEquals([], $attributeValues);
     }
@@ -29,6 +51,6 @@ class AttributeValuesExtractorTest extends TestCase
     {
         $this->filterMock = $this->createMock(FilterInterface::class, []);
         $this->numberFormatter = $this->createMock(NumberFormatter::class, []);
-        $this->attributeExtractor = new AttributeValuesExtractor($this->filterMock, $this->numberFormatter);
+        $this->attributeExtractor = new AttributeValuesExtractor(new TextFilter(), $this->numberFormatter);
     }
 }
