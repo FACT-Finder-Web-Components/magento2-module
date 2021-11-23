138dd0d64c5dcb0266eaa72722ae93ddffc9e4df
Pavel Novikov <paulfcdd@gmail.com>
2021-11-23 18:42:52 +0100

    FFWEB-2312: refactoring

--- src/Service/FeedFileService.php
+++ src/Service/FeedFileService.php
@@ -8,8 +8,8 @@ class FeedFileService
 {
     private const FEED_FILENAME_PATTERN = 'export.%type%.%channel%.csv';
 
-    public function getFeedExportFilename(string $type, string $channel): string
+    public function getFeedExportFilename(string $exportType, string $channel): string
     {
-        return str_replace(['%type%', '%channel%'], [$type, $channel], self::FEED_FILENAME_PATTERN);
+        return str_replace(['%type%', '%channel%'], [$exportType, $channel], self::FEED_FILENAME_PATTERN);
     }
 }
