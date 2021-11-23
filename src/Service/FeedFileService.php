1031cad9ba2be213b2414440be8d7f11d0e1967d
Pavel Novikov <paulfcdd@gmail.com>
2021-11-23 19:59:36 +0100

    FFWEB-2312: Add tests

--- src/Service/FeedFileService.php
+++ src/Service/FeedFileService.php
@@ -1,15 +1,31 @@
 <?php
 
+declare(strict_types=1);
 
 namespace Omikron\Factfinder\Service;
 
+use Exception;
 
 class FeedFileService
 {
     private const FEED_FILENAME_PATTERN = 'export.%type%.%channel%.csv';
 
+    /**
+     * @param string $exportType
+     * @param string $channel
+     * @return string
+     * @throws Exception
+     */
     public function getFeedExportFilename(string $exportType, string $channel): string
     {
+        if (empty($exportType)) {
+            throw new \Exception('Export type should not be empty');
+        }
+
+        if (empty($channel)) {
+            throw new \Exception('Channel should not be empty');
+        }
+
         return str_replace(['%type%', '%channel%'], [$exportType, $channel], self::FEED_FILENAME_PATTERN);
     }
 }
---
+++ src/Test/Unit/Service/FeedFileServiceTest.php
@@ -0,0 +1,51 @@
+<?php
+
+
+namespace Omikron\Factfinder\Service;
+
+use PHPUnit\Framework\TestCase;
+
+class FeedFileServiceTest extends TestCase
+{
+    /** @var FeedFileService */
+    private $feedFileService;
+
+    /**
+     * @dataProvider validDataProvider
+     * @throws \Exception
+     */
+    public function test_will_return_filename(string $exportType, string $channel, string $expected)
+    {
+        $result = $this->feedFileService->getFeedExportFilename($exportType, $channel);
+        $this->assertSame($expected, $result);
+    }
+
+    public function test_will_throw_exception_on_empty_export_type()
+    {
+        $this->expectException("Exception");
+        $this->expectExceptionMessage('Export type should not be empty');
+        $this->assertSame('export.test_type.test_channel.csv', $this->feedFileService->getFeedExportFilename('', 'test_channel'));
+    }
+
+    public function test_will_throw_exception_on_empty_export_channel()
+    {
+        $this->expectException("Exception");
+        $this->expectExceptionMessage('Channel should not be empty');
+        $this->assertSame('export.test_type.test_channel.csv', $this->feedFileService->getFeedExportFilename('test_type', ''));
+    }
+
+    public static function validDataProvider(): array
+    {
+        return [
+            [
+                'test_type', 'test_channel', 'export.test_type.test_channel.csv'
+            ]
+        ];
+    }
+
+    protected function setUp(): void
+    {
+        $this->feedFileService = new FeedFileService();
+
+    }
+}
