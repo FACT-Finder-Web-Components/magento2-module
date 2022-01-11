c0e5d3736cd738160c87bca5e46df490a16a2a9d
Pavel Novikov <paulfcdd@gmail.com>
2022-01-11 12:29:28 +0100

    FFWEB-2356: cr fix

--- src/Model/Adminhtml/System/Config/Source/Attribute.php
+++ src/Model/Adminhtml/System/Config/Source/Attribute.php
@@ -22,7 +22,7 @@ class Attribute implements OptionSourceInterface
     {
         $options = array_map(fn(EavAttribute $attribute) => ['value' => (string) $attribute->getAttributeCode(), 'label' => (string) $attribute->getDefaultFrontendLabel()], $this->collectionFactory->create()->getItems());
 
-        $options = array_filter($options, fn (array $a) => (bool)!!$a['label']);
+        $options = array_filter($options, fn(array $a) => (bool)!!$a['label']);
 
         usort($options, fn(array $a, array $b) => strtolower($a['label']) <=> strtolower($b['label']));
 
