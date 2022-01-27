af4d8f40fec4d07dd3dcea8f579460509f73a96f
Pavel Novikov <paulfcdd@gmail.com>
2022-01-27 13:20:58 +0100

    FFWEB-2371: remove else condition

--- src/Block/Ssr/RecordList.php
+++ src/Block/Ssr/RecordList.php
@@ -126,11 +126,9 @@ class RecordList extends Template
     {
         $isAbsoluteUrl = preg_match('%^(?:(?:https?|ftp)://)(?:\S+(?::\S*)?@|\d{1,3}(?:\.\d{1,3}){3}|(?:(?:[a-z\d\x{00a1}-\x{ffff}]+-?)*[a-z\d\x{00a1}-\x{ffff}]+)(?:\.(?:[a-z\d\x{00a1}-\x{ffff}]+-?)*[a-z\d\x{00a1}-\x{ffff}]+)*(?:\.[a-z\x{00a1}-\x{ffff}]{2,6}))(?::\d+)?(?:[^\s]*)?$%iu', $url);
 
-        if ($isAbsoluteUrl === 1) {
-            return true;
-        } else {
-            return false;
-        }
+        if ($isAbsoluteUrl === 1) return true;
+
+        return false;
     }
 
     private function removeForwardSlash(string $url): string
