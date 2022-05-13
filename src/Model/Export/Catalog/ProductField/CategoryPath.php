cc748e120fc0337ed2114b253dc75b4955ce688c
a.laurowski <ext-a.laurowski@omikron.net>
2022-05-13 07:49:21 +0200

    FFWEB-1764: category feed export

--- src/Model/Export/Catalog/ProductField/CategoryPath.php
+++ src/Model/Export/Catalog/ProductField/CategoryPath.php
@@ -5,25 +5,27 @@ declare(strict_types=1);
 namespace Omikron\Factfinder\Model\Export\Catalog\ProductField;
 
 use Magento\Catalog\Api\CategoryRepositoryInterface;
-use Magento\Catalog\Api\Data\CategoryInterface;
-use Magento\Framework\Exception\NoSuchEntityException;
-use Magento\Store\Model\Store;
-use Omikron\Factfinder\Api\Export\FieldInterface;
-use Magento\Framework\Model\AbstractModel;
 use Magento\Catalog\Model\Product;
+use Magento\Framework\Model\AbstractModel;
+use Omikron\Factfinder\Api\Export\FieldInterface;
+use Omikron\Factfinder\Model\Formatter\CategoryPathFormatter;
 
 class CategoryPath implements FieldInterface
 {
     /** @var CategoryRepositoryInterface */
     private $categoryRepository;
 
+    /** @var CategoryPathFormatter */
+    private $categoryPathFormatter;
+
     /** @var string */
     private $fieldName;
 
     public function __construct(CategoryRepositoryInterface $categoryRepository, string $fieldName = 'CategoryPath')
     {
-        $this->categoryRepository = $categoryRepository;
-        $this->fieldName          = $fieldName;
+        $this->categoryRepository    = $categoryRepository;
+        $this->fieldName             = $fieldName;
+        $this->categoryPathFormatter = new CategoryPathFormatter($this->categoryRepository);
     }
 
     public function getName(): string
@@ -38,49 +40,10 @@ class CategoryPath implements FieldInterface
      */
     public function getValue(AbstractModel $product): string
     {
-        $paths = array_map(function (int $categoryId) use ($product): array {
-            return $this->getPath($categoryId, $product->getStore());
+        $paths = array_map(function (int $categoryId) use ($product): string {
+            return $this->categoryPathFormatter->format($categoryId, $product->getStore());
         }, $product->getCategoryIds());
 
-        return implode('|', array_map(function (array $path): string {
-            return implode('/', array_map('urlencode', $path));
-        }, array_filter($paths)));
-    }
-
-    /**
-     * @param int   $categoryId
-     * @param Store $store
-     *
-     * @return string[]
-     */
-    private function getPath(int $categoryId, Store $store): array
-    {
-        try {
-            $storeId = (int) $store->getId();
-            return array_map(function (int $id) use ($storeId): string {
-                return trim($this->getCategory($id, $storeId)->getName());
-            }, $this->getPathIds($this->getCategory($categoryId, $storeId), $store));
-        } catch (NoSuchEntityException $e) {
-            return [];
-        }
-    }
-
-    private function getPathIds(CategoryInterface $category, Store $store): array
-    {
-        $path = explode('/', (string) $category->getPath());
-        $root = (int) ($path[1] ?? -1);
-        return $category->getIsActive() && $store->getRootCategoryId() == $root ? array_slice($path, 2) : [];
-    }
-
-    /**
-     * @param int $categoryId
-     * @param int $storeId
-     *
-     * @return CategoryInterface
-     * @throws NoSuchEntityException
-     */
-    private function getCategory(int $categoryId, int $storeId): CategoryInterface
-    {
-        return $this->categoryRepository->get($categoryId, $storeId);
+        return implode('|', array_filter($paths));
     }
 }
