7c104fffe9541e68dc1c91bdb18e079cc7775917
gjamroz <gjamroz@objectivity.co.uk>
2022-09-07 13:08:27 +0200

    FFWEB-2590 - fixed error while compiling magento2

--- src/Exception/ExportPreviewValidationException.php
+++ src/Exception/ExportPreviewValidationException.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace Omikron\Factfinder\Exception;
 
+use Throwable;
+
 class ExportPreviewValidationException extends \Exception
 {
-    public function __construct($message = '', $code = 0, \Throwable $previous = null)
+    public function __construct($message = '', $code = 0, Throwable $previous = null)
     {
         parent::__construct(sprintf('Product will not be exported. Reason: %s', $message), $code, $previous);
     }
