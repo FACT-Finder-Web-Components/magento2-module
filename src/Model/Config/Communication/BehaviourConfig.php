ac485cc7a5f7fe94134e72eaf0463410083dbc1e
robertsaternus <robert.saternus@gmail.com>
2024-05-28 12:24:02 +0200

    FFWEB-3063: Refactor parameter-whitelist
    Refactor parameter-whitelist

--- src/Model/Config/Communication/BehaviourConfig.php
+++ src/Model/Config/Communication/BehaviourConfig.php
@@ -23,8 +23,8 @@ class BehaviourConfig implements ParametersSourceInterface
     public function getParameters(): array
     {
         $parameters = [
-            'add-params'                  => $this->getAddParams(), // postStringifier
-            'parameter-whitelist'         => $this->getConfig(self::PATH_PARAMETER_WHITELIST), // allow setUrlParamOptionsListener
+            'add-params'          => $this->getAddParams(),
+            'parameter-whitelist' => $this->getParameterWhitelist(),
         ];
 
         return $parameters;
@@ -41,5 +41,15 @@ class BehaviourConfig implements ParametersSourceInterface
         $unserialized = array_values($this->serializer->unserialize($storedValue ?: '[]'));
 
         return array_column($unserialized, 'value', 'name');
+    }
+
+    private function getParameterWhitelist(): string
+    {
+        $storedValue  = $this->scopeConfig->getValue(self::PATH_PARAMETER_WHITELIST);
+        $unserialized = array_values($this->serializer->unserialize($storedValue ?: '[]'));
+        $params = array_column($unserialized, 'name');
+        array_walk($params, fn(&$x) => $x = "'$x'");
+
+        return rtrim(implode(', ', $params));
     }
 }
