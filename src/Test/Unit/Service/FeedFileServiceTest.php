5d8118e420f5f719cf5a5e000a818b826055c12e
robertsaternus <robert.saternus@gmail.com>
2024-06-14 15:33:18 +0200

    Fix tests

--- src/Test/Unit/Service/FeedFileServiceTest.php
+++ src/Test/Unit/Service/FeedFileServiceTest.php
@@ -28,21 +28,21 @@ class FeedFileServiceTest extends TestCase
     {
         $this->expectException(InvalidArgumentException::class);
         $this->expectExceptionMessage('Export type must not be empty');
-        $this->assertSame('exportData.test_type.test_channel.csv', $this->feedFileService->getFeedExportFilename('', 'test_channel'));
+        $this->assertSame('export.test_typeData.test_channel.csv', $this->feedFileService->getFeedExportFilename('', 'test_channel'));
     }
 
     public function test_will_throw_exception_on_empty_export_channel()
     {
         $this->expectException(InvalidArgumentException::class);
         $this->expectExceptionMessage('Channel must not be empty');
-        $this->assertSame('exportData.test_type.test_channel.csv', $this->feedFileService->getFeedExportFilename('test_type', ''));
+        $this->assertSame('export.test_typeData.test_channel.csv', $this->feedFileService->getFeedExportFilename('test_type', ''));
     }
 
     public static function validDataProvider(): array
     {
         return [
             [
-                'test_type', 'test_channel', 'export.test_type.test_channel.csv'
+                'test_type', 'test_channel', 'export.test_typeData.test_channel.csv'
             ]
         ];
     }
