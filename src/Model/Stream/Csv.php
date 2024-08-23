239514e3d39d139768016ff083a3978507e5b8b6
fritzmg <fmg@inspiredminds.at>
2024-08-23 09:26:47 +0100

    remove unnecessary isset

--- src/Model/Stream/Csv.php
+++ src/Model/Stream/Csv.php
@@ -38,7 +38,7 @@ class Csv implements StreamInterface
 
     private function getStream(): WriteInterface
     {
-        if (!isset($this->stream)) {
+        if ($this->stream) {
             $directory    = $this->filesystem->getDirectoryWrite(DirectoryList::VAR_DIR);
             $this->stream = $directory->openFile($directory->getAbsolutePath($this->filename), 'w+');
             $this->stream->lock();
