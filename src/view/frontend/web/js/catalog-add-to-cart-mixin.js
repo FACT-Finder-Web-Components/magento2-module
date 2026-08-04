2fdb9b30d83af22d32d91e5dcfce46dffaa12164
robertsaternus <ext-robert.saternus@fact-finder.com>
2026-08-04 13:42:58 +0200

    INT-347: Fix issue with cart btn if webc library is disabled
    Fix issue with cart btn if webc library is disabled

--- src/view/frontend/web/js/catalog-add-to-cart-mixin.js
+++ src/view/frontend/web/js/catalog-add-to-cart-mixin.js
@@ -1,40 +1,53 @@
 define([
     'jquery',
-    'factfinder',
     'underscore',
     'Magento_Customer/js/customer-data'
-], function ($, factfinder, _, customerData) {
+], function ($, _, customerData) {
     'use strict';
 
     $(document).on('ajax:addToCart', function (event, eventData) {
-        const cart = customerData.get('cart');
-        const productId = _.first(eventData.productIds);
-        let subscription;
 
-        if (productId && (!subscription || subscription.isDisposed)) {
-            subscription = cart.subscribe(function (cartData) {
-                const cartItem = _.find(cartData.items, function (item) {
-                    return item.product_id === productId;
-                });
-
-                const qtyInput = _.find($(eventData.form[0]).serializeArray(), function (element) {
-                    return element.name === 'qty';
-                });
-
-                if (qtyInput && cartItem) {
-                    const track = factfinder.tracking;
-                    track.cart([{
-                        id: cartItem.product_sku,
-                        price: parseFloat(cartItem.product_price_value),
-                        masterId: eventData.sku || cartItem.product_sku,
-                        count: parseInt(qtyInput.value),
-                        sid: JSON.parse(localStorage.ffwebco).sid
-                    }]);
-                }
-
-                subscription.dispose();
-            });
+        if (typeof window.factfinder === 'undefined') {
+            return;
         }
+
+        require(['factfinder'], function (factfinder) {
+            const cart = customerData.get('cart');
+            const productId = _.first(eventData.productIds);
+            let subscription;
+
+            if (productId && (!subscription || subscription.isDisposed)) {
+                subscription = cart.subscribe(function (cartData) {
+                    const cartItem = _.find(cartData.items, function (item) {
+                        return item.product_id === productId;
+                    });
+
+                    const qtyInput = _.find($(eventData.form[0]).serializeArray(), function (element) {
+                        return element.name === 'qty';
+                    });
+
+                    if (qtyInput && cartItem) {
+                        let sid = '';
+                        try {
+                            sid = JSON.parse(localStorage.getItem('ffwebco') || '{}').sid || '';
+                        } catch (e) {
+                            console.warn('FactFinder: Nie można pobrać SID z localStorage');
+                        }
+
+                        const track = factfinder.tracking;
+                        track.cart([{
+                            id: cartItem.product_sku,
+                            price: parseFloat(cartItem.product_price_value),
+                            masterId: eventData.sku || cartItem.product_sku,
+                            count: parseInt(qtyInput.value),
+                            sid: sid
+                        }]);
+                    }
+
+                    subscription.dispose();
+                });
+            }
+        });
     });
 
     return _.identity;
